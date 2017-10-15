74e5db4b1bf38acdc90f56aa314de72c6940d428
Vitaliy Tomochko <vitalik.tomochko@gmail.com>
2017-10-16 01:24:05 +0300

    add pwd/info

--- playlist.cpp
+++ playlist.cpp
@@ -3,13 +3,15 @@
 //#include " /usr/include/x86_64-linux-gnu/sys/stat.h"
 #include <stdlib.h>
 #include <sys/types.h>
+#include <sys/stat.h>
 #include <dirent.h>
 #include <errno.h>
 #include <vector>
 #include <cstring>
 #include <sys/param.h>
 #include <unistd.h>
-
+#include <fstream>
+#include "MP3tag.h"
 
 using namespace std;
 int getdir(string dir, vector<string> &files) {
@@ -46,53 +48,106 @@ void ls() {
 	 cout << files[i] << endl;
 	 }*/
 }
-void cd(char *theDir) {
-	DIR *dir = NULL;
-	struct dirent entry;
-	struct dirent *entryPtr = NULL;
-	int retval = 0;
-	unsigned count = 0;
-	char pathName[PATH_MAX + 1];
-
-	dir = opendir(theDir);
-	if (dir == NULL) {
-		cout << ("Error opening %s: %s", theDir);
+void cd(string path) {
+	int rc = chdir(path.c_str());
+	if (rc < 0) {
+		cout << "error" << endl;
 	}
-	retval = readdir_r(dir, &entry, &entryPtr);
-	while (entryPtr != NULL) {;
+	/*DIR *dir = NULL;
+	 struct dirent entry;
+	 struct dirent *entryPtr = NULL;
+	 int retval = 0;
+	 unsigned count = 0;
+	 char pathName[PATH_MAX + 1];
 
-		if ((strncmp(entry.d_name, ".", PATH_MAX) == 0)
-				|| (strncmp(entry.d_name, "..", PATH_MAX) == 0)) {
-			retval = readdir_r(dir, &entry, &entryPtr);
-			continue;
-		}
+	 dir = opendir(theDir);
+	 if (dir == NULL) {
+	 cout << ("Error opening %s: %s", theDir);
+	 }
+	 retval = readdir_r(dir, &entry, &entryPtr);
+	 while (entryPtr != NULL) {
+	 ;
+
+	 if ((strncmp(entry.d_name, ".", PATH_MAX) == 0)
+	 || (strncmp(entry.d_name, "..", PATH_MAX) == 0)) {
+	 retval = readdir_r(dir, &entry, &entryPtr);
+	 continue;
+	 }
+	 }*/
+}
+void pwd() {
+	char buffer[MAXPATHLEN];
+	char *path = getcwd(buffer, MAXPATHLEN);
+	if (!path) {
+		cout << "error" << endl;
+	} else {
+		string CurrentPath;
+		CurrentPath = path;
+		cout << CurrentPath << endl;
 	}
 }
+int readmp3(const char *path) {
+	 ofstream fout("info.txt");
+	int fileNameLength = 1024;
+	int mp3TagSize = 128;
+	TAGdata tagStruct;
+	//char *fileName=new char[fileNameLength + 1];
+	char* fileName=new char[fileNameLength+1];
+	fileName=(char*)path;
+	ifstream mp3File;
+	char buffer[mp3TagSize + 1];
+	//strcpy(path, fileName.c_str());
+	//cout<<fileName;
 
+	//cin.getline(fileName, fileNameLength);
+	mp3File.open(path);
+	buffer[mp3TagSize] = '\0';  //just a precaution
+
+	if (mp3File) {
+		cout << "Successful open of " << fileName << endl;
+	} else {
+		cerr << " Could not open " << fileName << endl;
+		return 1;
+	}
+
+	mp3File.seekg(-(mp3TagSize), ios::end);
+	if (mp3File.fail()) {
+		cerr << "Could not seek " << endl;
+		return 1;
+	}
+
+	mp3File.read(reinterpret_cast<char *>(&tagStruct), mp3TagSize);
+	if (mp3File.fail()) {
+		cerr << "Could not read after seeking" << endl;
+		return 1;
+	}
+	fout <<"tag:"<< tagStruct.tag << endl;
+	fout <<"title:"<< tagStruct.title << endl;
+	fout <<"artist:"<< tagStruct.artist << endl;
+	fout <<"album:"<< tagStruct.album << endl;
+	fout <<"year:"<< tagStruct.year << endl;
+	fout <<"comment:"<< tagStruct.comment << endl;
+	fout <<"genre:"<< tagStruct.genre << endl;
+	fout <<endl;
+	mp3File.close();
+}
 int main() {
-	string command, path;
+	string command;
 	while (command != "exit") {
 		getline(cin, command);
 		if (command.find("ls") == 0) {
 			ls();
 		}
-		if (command == "curr") {
-			char buffer[MAXPATHLEN];
-			char *path = getcwd(buffer, MAXPATHLEN);
-			if (!path) {
-			cout<<"error"<<endl;
-			} else {
-				string CurrentPath;
-				CurrentPath = path;
-				cout << CurrentPath << endl;
-			}
-		} else if (command == "cd") {
-			string newDir;
-			cin >> newDir;
-			int rc = chdir(newDir.c_str());
-			if (rc < 0) {
-				cout<<"error"<<endl;
-			}
+		if (command == "pwd") {
+			pwd();
+		} else if (command.find("cd") == 0) {
+			cd(command.substr(command.find(" ") + 1));
+
+		}
+		if (command.find("info") == 0) {
+		//	const char *d=();
+		//	cout<<command.substr(command.find(" ") + 1).c_str();
+			readmp3(command.substr(command.find(" ") + 1).c_str());
 		}
 
 	}
